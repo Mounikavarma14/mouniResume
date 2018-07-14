08dcde224327a380d9a19f09e1b0ca41a5e81654
Mounikavarma14 <mounika.chekuri14@gmail.com>
2018-07-14 15:12:34 +0530

    add

--- main.js
+++ main.js
@@ -3,13 +3,65 @@ var xhr=new XMLHttpRequest();
 xhr.overrideMimeType("application/json");
 xhr.open("GET",file,true);
 xhr.onreadystatechange=function(){
-if(xhr.readystate===4 && xhr.status=="200");
+if(xhr.readyState===4 && xhr.status=="200"){
 	callback(xhr.responseText);
 }
 };
 xhr.send();
+}
 
 loadjson("data.json",function(text){
 	var data=JSON.parse(text);
 	console.log(data);
+	career(data.career);
+	education(data.education);
+	skills(data.skills);
 	})
+var childTwo=document.querySelector(".childtwo");
+
+	function career(careerInfo){
+		var heading=document.createElement("h2")
+		heading.textContent="Career Objective";
+		childTwo.appendChild(heading);
+		var hr=document.createElement("hr");
+		heading.appendChild(hr);
+var careerP=document.createElement("p");
+careerP.textContent=careerInfo.info;
+childTwo.appendChild(careerP);
+}
+
+
+function education(edu){
+	console.log(edu.length);
+var table=document.createElement("table");
+childTwo.appendChild(table);
+table.border="1"
+var tr1="<tr><td>Degree</td><td>Institute</td><td>Data</td></tr>"
+table.innerHTML=tr1;
+var tr2="";
+for(var i=0; i<edu.length; i++){
+	tr2+="<tr><td>"+edu[i].degree+"</td><td>"+edu[i].institute+"</td><td>"+edu[i].data+"</td></tr>";
+}
+table.innerHTML=tr1+tr2;
+}
+function skills(skil)
+{
+	var heading=document.createElement("h2");
+	heading.textContent="Skill-Set";
+	childTwo.appendChild(heading);
+	var hr=document.createElement("hr");
+	heading.appendChild(hr);
+	for(var i=0; i< skil.length; i++)
+	{
+		var titleH=document.createElement("h3");
+		titleH.textContent=skil[i].title;
+		childTwo.appendChild(titleH);
+		var ul=document.createElement("ul");
+		childTwo.appendChild(ul);
+		for(var j=0; j<skil[i].info.length; j++){
+		var li=document.createElement("li");
+		li.textContent=skil[i].info[j];
+		ul.appendChild(li);
+	}
+}
+}
